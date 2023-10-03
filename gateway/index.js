7ac5b43047530106cc7c91fb5f0872496f412214
SebasUNLu <SebasUNLu@gmail.com>
2023-10-02 21:44:08 -0300

    hot fix

--- gateway/index.js
+++ gateway/index.js
@@ -8,21 +8,34 @@ const PORT = 8000;
 
 app.use(morgan("dev"));
 
-app.use("/characters", createProxyMiddleware({
-  target: "http://characters:8001",
-  // Modifica los headers de la request para que vaya al nuevo microservicio
-  changeOrigin: true
-}));
+app.use(
+  "/characters",
+  createProxyMiddleware({
+    target: "http://characters:8001",
+    // Modifica los headers de la request para que vaya al nuevo microservicio
+    changeOrigin: true,
+  })
+);
 
-app.use("/films", createProxyMiddleware({
-  target: "http://films:8002",
-  changeOrigin: true
-}));
+app.use(
+  "/films",
+  createProxyMiddleware({
+    target: "http://films:8002",
+    changeOrigin: true,
+  })
+);
 
-app.use("/planets", createProxyMiddleware({
-  target: "http://planets:8003",
-  changeOrigin: true
-}));
+app.use(
+  "/planets",
+  createProxyMiddleware({
+    target: "http://planets:8003",
+    changeOrigin: true,
+  })
+);
+
+app.use("*", (req, res) => {
+  res.status(404).json({ message: "Ruta no encontrada" });
+});
 
 app.listen(PORT, () => {
   console.log("Gateway on port 8000");
